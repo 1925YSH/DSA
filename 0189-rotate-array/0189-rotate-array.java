6ff5a4abcba5ce7f54c0b843b238c8123b203004
YASH OTWAL <128904224+1925YSH@users.noreply.github.com>
2026-09-11 15:57:37 +0530

    Time: 4 ms (43.81%), Space: 266.1 MB (40.31%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.java
@@ -0,0 +1,26 @@
+class Solution {
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+
+        k = k % n;
+        int temp []= new int [k];
+
+        //first stored value in temp array
+        for(int i=0;i<k;i++){
+            temp[i]=nums[n-k+i];
+                }
+
+//second shifting remaining to right
+
+        for(int i =n-k-1;i>=0;i--){
+            nums[i+k] = nums[i];
+        }
+
+ //Saving temp array elements to begining       
+
+        for(int i =0;i<k;i++){
+            nums[i]=temp[i];
+        }
+
+    }
+}
